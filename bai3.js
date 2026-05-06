7f75458b5d7cff07d4257d0fce197a087b8e530d
dinhahhh <anhdinh5991@gmail.com>
2026-05-06 20:59:59 +0700

    bai3

---
+++ bai3.js
@@ -0,0 +1,35 @@
+let number = prompt("Nhập số từ 0 đến 9")
+switch(number){
+case "0" :
+alert("Số không");
+break;
+case "1" :
+alert("Số một");
+break;
+case "2" :
+    alert("Số hai");
+    break;
+case "3" :
+    alert("Số ba");
+    break;
+case "4" :
+    alert("Số bốn");
+    break;
+case "5" :
+    alert("Số năm");
+    break;
+case "6" :
+    alert("Số sáu");
+    break;
+case "7" :
+    alert("Số bảy");
+    break;
+case "8" :
+    alert("Số tám");
+    break;
+case "9" :
+    alert("Số chín");
+    break;
+default:
+    alert("Chưa có số này")
+}
